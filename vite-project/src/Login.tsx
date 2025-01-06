4ebc2b91b927d1159473e13def0b32a5d2c43609
BifeFr1t0 <matheus0cavalcanti002@gmail.com>
2025-01-05 22:44:10 -0300

    Header

---
+++ vite-project/src/Login.tsx
@@ -0,0 +1,13 @@
+import './Login.css'
+
+function LoginPage(){
+    return(
+        <>
+        <div>
+            <h1>a</h1>
+        </div>
+        </>
+    )
+}
+
+export default LoginPage
--- vite-project/src/components/login/login.tsx
+++ vite-project/src/components/login/login.tsx
@@ -1,11 +1,22 @@
 import './login.css'
+import logo from './foto.png'
 
 function Login(){
     return (
         <>
-            <div>
-                <h1>oi</h1>
-            </div>
+        <div className="container">
+            <header>
+                <div className="logo">
+                    <img src={logo} alt="logo da lista de afazeres"/>
+                    <h2>TO DO LIST</h2>
+                </div>
+                <div className="login">
+                    <a className="entrar" href='../../Login.tsx'>
+                        Entrar
+                    </a>
+                </div>
+            </header>
+        </div>
         </>
     )
 }
